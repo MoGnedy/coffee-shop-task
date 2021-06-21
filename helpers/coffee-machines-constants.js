804e6b952a8f201b9ecab316016a773204d30964
mognedy <mognedy@gmail.com>
2021-06-21 20:21:48 +0200

    Add coffeeMachinesProductTypes as constant

---
+++ helpers/coffee-machines-constants.js
@@ -0,0 +1,7 @@
+const coffeeMachinesProductTypes = {
+  COFFEE_MACHINE_LARGE: "COFFEE_MACHINE_LARGE",
+  COFFEE_MACHINE_SMALL: "COFFEE_MACHINE_SMALL",
+  ESPRESSO_MACHINE: "ESPRESSO_MACHINE",
+};
+
+module.exports = { coffeeMachinesProductTypes };
